2f658dec0d2e155bbc571b9ff134d2ec6a2e028e
Hao Jin <jinhaomirror@gmail.com>
2024-09-19 18:42:07 -0500

    added movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,13 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+    :release_date => '13-Aug-2021'},
+  {:title => 'Me and Earl and the Dying Girl', :rating => 'PG-13',
+    :release_date => '25-Jan-2015'},
+  {:title => 'American History X', :rating => 'R',
+    :release_date => '1-Jul-1998'},
+  {:title => 'The Enigma of Kasper Hauser', :rating=> 'Not Rated',
+    :release_date =>'1-Nov-1974'}
 ]
 
 more_movies.each do |movie|
